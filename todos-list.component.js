d1a91e7dab2e83332a94fed9dbae9cb4a7bb4f57
Samuel Adama-Doh <samueladamadoh@gmail.com>
2019-12-30 09:35:31 +0000

    todoList updated

--- todos-list.component.js
+++ todos-list.component.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom"
-import axios from "axios"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Todo = props => (
   <tr>
@@ -8,36 +8,39 @@ const Todo = props => (
     <td>{props.todo.todo_responsible}</td>
     <td>{props.todo.todo_priority}</td>
     <td>
-      <Link to={"/edit/"+props.todo._id}>Edit</Link>
+      <Link to={"/edit/" + props.todo._id}>Edit</Link>
     </td>
   </tr>
-)
+);
 
 export default class TodosList extends Component {
   constructor(props) {
-    super(props)
-
-    this.setState = { todos: [] }
+    super(props);
+    this.state = { todos: [] };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/todos/')
-          .then(res => {
-            this.setState({ todos: Response.data })
-          })
-          .catch((error) => console.log(error))
+    axios
+      .get("http://localhost:4000/todos/")
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   todoList() {
-    return this.state.todos.map((currentTodo, i) => 
-    <Todo todo={currentTodo} key={i} />)
+    return this.state.todos.map(function(currentTodo, i) {
+      return <Todo todo={currentTodo} key={i} />;
+    });
   }
 
   render() {
-    return(
+    return (
       <div>
-        <h3>Todos TodosList</h3>
-        <table className="table table-stripped" style={{ marginTop: 20 }}>
+        <h3>Todos List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
           <thead>
             <tr>
               <th>Description</th>
@@ -46,11 +49,9 @@ export default class TodosList extends Component {
               <th>Action</th>
             </tr>
           </thead>
-          <tbody>
-            { this.TodosList() }
-          </tbody>
+          <tbody>{this.todoList()}</tbody>
         </table>
       </div>
-    )
+    );
   }
 }
